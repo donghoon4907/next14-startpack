c1c0f69a07b78212759ae465991dbe0475a9c37b
donghoon4907 <wre789812@naver.com>
2023-12-13 21:29:09 +0900

    Sass 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
-import './globals.css';
+import '@my/styles/main.scss';
 
 const NotoSansKr = Noto_Sans_KR({
     weight: ['100', '300', '400', '500', '700', '900'],
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+/**
+ * 빌드 설정 가이드
+ *
+ * 1. Sass 사용 시 의존성 추가 필수
+ *
+ */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    sassOptions: {
+        includePaths: ['./styles'],
+    },
 };
 
 module.exports = nextConfig;
